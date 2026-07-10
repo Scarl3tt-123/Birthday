42e115b018b1626b667a5ea10166853796082b45
Scarl3tt-123 <scarlettoliveira080609@gmail.com>
2026-07-10 17:03:35 +0100

    renamed:    Styles.css -> WebApplication1/Styles.css 	renamed:    index.html -> WebApplication1/index.html 	modified:   WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs 	modified:   WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfoInputs.cache 	renamed:    script.js -> WebApplication1/script.js

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff21a28f0c01ca3751c6a861e012ac9e0caf16e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
