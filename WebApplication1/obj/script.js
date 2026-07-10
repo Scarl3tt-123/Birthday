46cbf8d41c09a95b6eabbde231ae996cae58abaa
Scarl3tt-123 <scarlettoliveira080609@gmail.com>
2026-07-10 16:56:36 +0100

    initial commit

---
+++ WebApplication1/obj/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+function showpage2() {
+    document.getElementById("page1").style.display= "none";
+    document.getElementById("page2").style.display="block";
+}
+function showpage1() {
+    document.getElementById("page1").style.display= "block";
+    document.getElementById("page2").style.display="none";
+}
+
+
